037d1ee3f2f6ecb149b5b05ef0467b7c56e84046
Swathi K <thesde.swathi1215@gmail.com>
2025-11-17 14:37:01 +0530

    Time: 4 ms (100%), Space: 44 MB (82.85%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int t = x;
+        int r = 0;
+        while(t>0){
+            r=(r*10)+t%10;
+            t/=10;
+        }
+        return x==r;
+    }
+}
