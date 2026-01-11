1c716ba591eea9f5d2420ba017727bff73a9c911
Swathi K <thesde.swathi1215@gmail.com>
2026-01-11 12:47:10 +0530

    Time: 0 ms (100%), Space: 61.3 MB (58.53%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -3,9 +3,9 @@ class Solution {
         //right totation
         int n = nums.length;
         k = k%n;
-        reverse(nums,n-k,n-1);
-        reverse(nums,0,n-k-1);
         reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
     }
     public static void reverse(int nums[], int s, int e)
     {
