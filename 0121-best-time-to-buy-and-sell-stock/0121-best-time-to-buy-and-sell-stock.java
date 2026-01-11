4a6656a44c49ac9ee2abb182b3fd423d640ce3da
Swathi K <thesde.swathi1215@gmail.com>
2026-01-11 12:31:12 +0530

    Time: 1 ms (99.72%), Space: 94.7 MB (6.39%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -3,11 +3,8 @@ class Solution {
         int min = Integer.MAX_VALUE;
         int max = 0;
         for(int i=0;i<a.length;i++){
-            if(a[i]<min){
-                min = a[i];
-            }else if(a[i]-min>max){
-                max = a[i]-min;
-            }
+            if(a[i]<min) min = a[i];
+            else if(a[i]-min>max) max = a[i]-min;
         }
         return max;
     }
