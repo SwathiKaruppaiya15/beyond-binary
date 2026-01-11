4df907ce10f2584b918dde54b1c1649413a792c9
Swathi K <thesde.swathi1215@gmail.com>
2026-01-11 13:46:43 +0530

    Time: 4 ms (79.02%), Space: 46.6 MB (26.37%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        if(n!=m) return false;
+        char ch1[] = s.toCharArray();
+        char ch2[] = t.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        for(int i=0;i<Math.min(n,m);i++){
+            if(ch1[i]!=ch2[i]) return false;
+        }
+        return true;
+    }
+}
