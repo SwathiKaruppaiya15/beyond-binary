4cd9eca3279f8dc6a9bb9e49ca9f3dd4975dfc59
Swathi K <thesde.swathi1215@gmail.com>
2026-03-09 11:15:02 +0530

    Time: 4 ms (81.22%), Space: 46.7 MB (14.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,6 +5,9 @@ class Solution {
         char ch2[] = t.toCharArray();
         Arrays.sort(ch1);
         Arrays.sort(ch2);
-        return Arrays.equals(ch1,ch2);
+        for(int i=0;i<ch1.length;i++){
+            if(ch1[i]!=ch2[i]) return false;
+        }
+        return true;
     }
 }
